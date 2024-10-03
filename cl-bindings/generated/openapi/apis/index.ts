4d553ad36ea3dbf7895bdf45710432dded900d1c
Jack Sanders <jacklewissanders@gmail.com>
2024-10-03 18:17:11 -0500

    minor

--- cl-bindings/generated/openapi/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ClBackendroutesclimbsclimbsControllerApi';
-export * from './ClBackendrouteslocationslocationsControllerApi';
-export * from './ClBackendroutespreferencespreferencesControllerApi';
-export * from './ClBackendroutesseshesseshesControllerApi';
